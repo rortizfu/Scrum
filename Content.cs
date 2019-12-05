d57a1d7db04ecc9cedd188070dcd9585732bf7d5
rortizfu <36171837+rortizfu@users.noreply.github.com>
2019-12-05 18:23:57 -0500

    Add files via upload

---
+++ Content.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crediflores.CAL.DTO.Page
+{
+    public class Content
+    {
+
+        #region Propierties
+
+        /*Content*/
+        public long ContentId { get; set; }
+        public long TypeContentId { get; set; }
+        public string ContentName { get; set; }
+        public string ContentDescription { get; set; }
+        public long ContentOrder { get; set; }
+        public long? ContentIdParent { get; set; }
+        public Boolean ContentState { get; set; }
+        public long Component { get; set; }
+
+        public long ComponentCantVarText { get; set; }
+        public long ComponentCantVarImage { get; set; }
+
+
+        public Section section { get; set; }
+        #endregion
+    }
+}
